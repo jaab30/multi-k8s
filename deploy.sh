ea9dcac0b5d8b8881f1c80b35f96f56a5ea6a4cf
jaab30 <jaab30@yahoo.com>
2021-04-02 17:35:38 -0400

    fixed deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ docker push jaab30/multi-worker:latest
 docker push jaab30/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jaab30/multi-server:$SHA
-kubectl set image deployments/client-deployment client=jaab30/multi-server:$SHA
-kubectl set image deployments/server-deployment server=jaab30/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jaab30/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jaab30/multi-worker:$SHA
