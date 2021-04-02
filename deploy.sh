2e30b86edf85b3e45a2aae0369c1d69ebf2ac611
jaab30 <jaab30@yahoo.com>
2021-04-02 17:20:19 -0400

    Deploy.sh and final travis scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t jaab30/multi-client:latest -t jaab30/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jaab30/multi-server:latest -f jaab30/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t jaab30/multi-worker:latest -f jaab30/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker push jaab30/multi-client:latest
+docker push jaab30/multi-client:$SHA
+docker push jaab30/multi-server:latest
+docker push jaab30/multi-server:$SHA
+docker push jaab30/multi-worker:latest
+docker push jaab30/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jaab30/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jaab30/multi-server:$SHA
+kubectl set image deployments/server-deployment server=jaab30/multi-server:$SHA
